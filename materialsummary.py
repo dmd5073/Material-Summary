31615ebeac39b2bbf6cf94f4f1c3e3164662de0e
xubuntu 12.04 <wes@ubuntu.(none)>
2013-02-23 20:32:10 -0800

    stared by reading standard format

--- materialsummary.py
+++ materialsummary.py
@@ -1,3 +1,34 @@
 import csv
-def main (inputfilename,outfilename):
-    
+import sys
+
+def main (inputfilename,outputfilename):
+    print inputfilename
+    print outputfilename
+
+    csvfile = open(inputfilename)
+    reader = csv.reader(csvfile)
+
+    qtyIdx = 0
+    measurementIdx = 1
+    descIdx = 2
+    markNoIdx = 3
+
+    for line in reader:
+        print "        qty:"+str(line[qtyIdx])
+        print "measurement:"+str(line[measurementIdx])
+        print "       desc:"+str(line[descIdx])
+        print "     markNo:"+str(line[markNoIdx])
+        print ""
+
+inputFileNameArg = 1
+outputFileNameArg = 2
+minargs = 3
+
+if(len(sys.argv)<minargs):
+  print "NOT ENOUGH ARGS PROVIDED, MIN ARGS IS "+str(minargs)
+  print "proper format is 'python proc_materials_summary.py <filename>'"
+  exit()
+
+main(sys.argv[inputFileNameArg], sys.argv[outputFileNameArg])
+
+
