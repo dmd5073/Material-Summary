c2e8827e02a80bbda679abbee3ba6c3da114ed48
xubuntu 12.04 <wes@ubuntu.(none)>
2013-03-02 17:38:40 -0800

    made diffing easier

--- materialsummary.py
+++ materialsummary.py
@@ -1,5 +1,6 @@
 import csv
 import sys
+import math
 
 class record :
     def __init__(self,qty,measurement,desc):
@@ -27,16 +28,12 @@ def main (inputfilename,outputfilename):
 
     for line in reader:
         qty = line[qtyIdx]
-        measurement = str(line[measurementIdx])
-        desc = str(line[descIdx])
-        markNo = line[markNoIdx]
-        try:
-            if markNo.lower() == "x":
-                continue;
-            markNo = int(markNo)
-        except ValueError, TypeError:
-            print "mark number:"+str(markNo)+" wasn't valid.  I QUIT!"
-            sys.exit()
+        measurement = text(line[measurementIdx]).upper()
+        desc = text(line[descIdx]).upper()
+
+        if(line[markNoIdx].lower()=="x"):
+            continue
+        markNo = text(line[markNoIdx]).upper()
 
         print "        qty:"+str(qty)
         print "measurement:"+str(measurement)
@@ -49,15 +46,8 @@ def main (inputfilename,outputfilename):
             fileData[markNo] = myRecord 
         else:
             print "not new mark number"
-            if myRecord.desc!=fileData[markNo].desc:
-                which = getInt("Which desc would you like to use?\n1)"+myRecord.desc+"\n2)"+fileData[markNo].desc, 1, 2)
-                if(which==1):
-                    fileData[markNo].desc = myRecord.desc
-
-            if(myRecord.measurement!=fileData[markNo].measurement):
-                which = getInt("Which measurement would you like to use?\n1)"+myRecord.measurement+"\n2)"+fileData[markNo].measurement, 1, 2)
-                if(which==1):
-                    fileData[markNo].measurement = myRecord.measurement
+            decideOption("Which desc would you like to use?", myRecord.desc, fileData[markNo].desc)
+            decideOption("Which measurement would you like to use?", myRecord.measurement, fileData[markNo].measurement)
 
             fileData[markNo].qty = fileData[markNo].qty + myRecord.qty
         
@@ -68,7 +58,43 @@ def main (inputfilename,outputfilename):
     for key in fileData:
         value = fileData[key]
         writer.writerow([value.qty, value.measurement, value.desc, key])
+def text(string):
+    string = str(string)
+    if len(string)==0 or (string[0]!="'" and not (string[0]=='"' and string[len(string)-1]=='"')):
+        string = "'"+string
+    return string
+
+def decideOption(prompt, opt1, opt2):
+    if(opt1==opt2):
+        return opt1
+    else: 
+        if len(opt1)>len(opt2):
+            opt2 = opt2.ljust(len(opt1))
+        if len(opt1)<len(opt2):
+            opt1 = opt1.ljust(len(opt2))
+
+        differences = ""
+        for i in range(len(opt1)):
+            if opt1[i] != opt2[i]:
+                differences = differences + "^"
+            else:
+                differences = differences + " "
+
+        print prompt
         
+        width = 40
+        numLines = int(math.ceil((float(len(opt1)))/width))
+        for i in range(numLines):
+            print "  1):"+opt1[i*width:(i+1)*width]
+            print "  2):"+opt2[i*width:(i+1)*width]
+            print "diff:"+differences[i*width:(i+1)*width]
+
+        decision = getInt(prompt, 1, 2)
+        if decision == 1:
+            return opt1
+        else:
+            return opt2
+
 def getInt(prompt, minVal=0, maxVal=sys.maxint):
     found = False
     while(not found):
